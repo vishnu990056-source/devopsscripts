3cba0c2c59a6d29b44af764ff711ab589f2f1a10
vishnu <vishnu.990056@gmail.com>
2026-08-24 20:46:44 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,11 @@
 dnf install java-21-amazon-corretto -y
-wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.14/bin/apache-tomcat-11.0.14.tar.gz
-tar -zxvf apache-tomcat-11.0.14.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.14/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.14/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="root123456" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.14/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.14/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-11.0.14/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-11.0.14/webapps/manager/META-INF/context.xml
-sed -i '22d' apache-tomcat-11.0.14/webapps/manager/META-INF/context.xml
-sh apache-tomcat-11.0.14/bin/startup.sh
+wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.25/bin/apache-tomcat-11.0.25.tar.gz
+tar -zxvf apache-tomcat-11.0.25.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.25/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.25/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="root123456" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.25/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.25/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-11.0.25/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-11.0.25/webapps/manager/META-INF/context.xml
+sed -i '22d' apache-tomcat-11.0.25/webapps/manager/META-INF/context.xml
+sh apache-tomcat-11.0.25/bin/startup.sh
